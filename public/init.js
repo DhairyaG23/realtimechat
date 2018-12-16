d2807a8289e64836501e108b2665e8294da5bc88
DhairyaG23 <dhairyag23@gmail.com>
2018-12-16 10:26:35 -0600

    Add files via upload

---
+++ public/init.js
@@ -0,0 +1,16 @@
+if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+firebase.initializeApp({
+
+
+
+
+
+
+
+  "apiKey": "AIzaSyAQ1eLXXOEUAQlIqI9C3YmsQG73UmG-Wo8",
+    "authDomain": "dhairyag23.github.io/realtime/__/auth/handler",
+    "databaseURL": "https://amazingchatdg23.firebaseio.com",
+    "projectId": "amazingchatdg23",
+    "storageBucket": "amazingchatdg23.appspot.com",
+    "messagingSenderId": "597980697995"
+});
